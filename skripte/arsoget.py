2ed8d88af26fa3b56b8a5987cd8e78d02bac0812
zigazupancic <zigazupancic5@gmail.com>
2016-01-29 21:09:55 +0100

    Dodana funkcija: dodaj_v_csv.

--- skripte/arsoget.py
+++ skripte/arsoget.py
@@ -1,7 +1,7 @@
 import requests
 import re
 import time
-
+import csv
 
 def pridobi_arso_podatke():
     data = requests.get("http://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/"
@@ -9,7 +9,7 @@ def pridobi_arso_podatke():
     data = data.text
     result = re.findall(r'<td class="meteoSI-th">\w*,\s(\d\d\.\d\d\.\d\d\d\d\s\d\d:\d\d).*id="t">(-?\d+.?\d*)</td>.*'
                     r'id="rh">(\d+)</td>.*</td>', data)
-    return result
+    return reversed(uredi_podatke(result))
 
 
 def uredi_podatke(podatki_iz_strani):
@@ -25,3 +25,20 @@ def pretvori_niz_v_cas(niz):
 
 def pretvori_cas_v_niz(cas):
     return "{}.{}.{} {}:{}".format(cas.tm_mday, cas.tm_mon, cas.tm_year, cas.tm_hour, cas.tm_min)
+
+def dodaj_v_csv(podatki):
+    """Ustvari datoteko arso.csv, če ne obstaja in doda podatke, ki še niso v datoteki"""
+    with open('arso.csv', 'a+') as csvdatoteka:
+        csvdatoteka.seek(0)
+        obstojeci_podatki = csv.reader(csvdatoteka)
+        seznam_obstojecih_podatkov = []
+        for podatek in obstojeci_podatki:
+            seznam_obstojecih_podatkov.append(podatek)
+
+        # TODO: Dodaj preverjanje, če je podatek že v csv datoteki in dodaj le tiste, ki niso
+
+
+        vrste_podatkov = ['cas', 'temperatura', 'vlaznost']
+        csv_pisi = csv.DictWriter(csvdatoteka, fieldnames=vrste_podatkov)
+        for podatek in podatki:
+            csv_pisi.writerow({'cas': podatek[0], 'temperatura': podatek[1], 'vlaznost': podatek[2]})
